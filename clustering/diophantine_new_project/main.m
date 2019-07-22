d5e8759767a37449ff0b2a038ae2358f48b0c284
Aditya <42856317+aditya1453@users.noreply.github.com>
2019-07-22 12:57:10 +0700

    create main function

---
+++ clustering/diophantine_new_project/main.m
@@ -0,0 +1,66 @@
+%% Program Description
+% SPO - Clustering Solve Diophantine Equation
+% @author : Yudi Aditya
+% Email   : yudi.aditya.89@gmail.com
+
+%% MAIN PROGRAM
+clc
+clear
+
+%% Fungsi Objektif
+% 1. Persamaan linear Diophantine
+% 2. Luca dan Soydan
+% 3. Cangul dkk
+% 4. Perez dkk
+% 5. Perez dkk
+% 6. Amaya
+% 7. Pell, Ai dkk
+% 8. Markoff
+% 9. Page 6 No 8
+% 10. Page 6 No 9
+% 11. Page 7 No 2
+% 12. Page 7 No 8
+
+kasus       = 10 ;
+
+%% Data Algoritma
+m_cluster   = 1000; %Number of search point
+gamma       = 0.1;  %Cut off Parameter for function Fx
+k_cluster   = 20;   %Maximum number of iteration at the clustering phase
+theta       = 30;   %Spiral rotation
+r           = 0.95;  %Spiral ratio
+m           = 100;  %Number of search point
+k_max       = 50;  %Number of iteration
+epsilon     = 10^-5;%Parameter of root acceptance
+delta       = 0.01;  %Parameter to distinguish between root
+
+f           = operasi;
+tic
+%% Tools
+[n, min_x, max_x] = f.data(kasus);
+S = eye (n);
+for i=1:n-1
+    for j=1:i
+        R = f.matriksR(n-i,n+1-j,theta, n);
+        S = S*R;
+    end
+end
+S =S*r;
+
+root =[];
+fitness_fx = [];
+
+%% Subroutine 1 : Clustering
+[c_cluster, domain_max, domain_min] = f.clustering(m_cluster, k_cluster, S, gamma,n, max_x, min_x, kasus);
+[row,col] = size (c_cluster);
+
+% Subroutine 2 : Spiral Process
+for i=1:row
+    [optimum_x, optimum_fx]   = f.spo(S, n, k_max, max_x, min_x, domain_max(i,:), domain_min(i,:), m, epsilon, root, delta, kasus);
+    
+    % Subroutine 3 : Selection
+    [root, fitness_fx] = f.selection (optimum_x, optimum_fx, fitness_fx, root, epsilon, delta, kasus);
+end
+toc
+
+root
